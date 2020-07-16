7d7e78a13f969b2af590d2bdd65f90a41a3a87de
Waswa Rodgers <waswam1994@gmail.com>
2020-07-16 09:39:03 +0300

    Performed a payment formula to compute the mortgage calculations

--- mortgage.loan.js
+++ mortgage.loan.js
@@ -1,7 +1,8 @@
 function calculateMortgage(p, r, n) {
 	r = convertPercentToDecimal(r);
 	n = convertYearsToMonths(n);
-	return r;
+	var pay = (p * r) / (1 - (Math.pow((1 + r), -n)));
+	return parseFloat(pay).toFixed(2);
 }
 
 console.log(calculateMortgage(4000, 18, 2));
