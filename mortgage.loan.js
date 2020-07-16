7ba442e00bfebd9892d270ea27097f53288af211
Waswa Rodgers <waswam1994@gmail.com>
2020-07-16 09:22:12 +0300

    Created convertPercentToDecimal and convertYearsToMonts functions

--- mortgage.loan.js
+++ mortgage.loan.js
@@ -1,3 +1,13 @@
 function calculateMortgage(p, r, n) {
-	
+
 }
+
+function convertPercentToDecimal(percent) {
+	return (percent / 100) / 12;
+}
+
+function convertYearsToMonths(years) {
+	return years * 12;
+}
+
+console.log(convertYearsToMonths(2));
